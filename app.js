b98cb7c7efa91589889d56b63c0488eb32d6075f
Mahad Ali <amahad427@gmail.com>
2024-02-24 01:35:58 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+module.exports= {
+
+x:10,
+y:20,
+z:function(a=20,b=10){
+    return a-b;
+}
+
+}
---
+++ index.js
@@ -0,0 +1,27 @@
+//const app = require('./app')
+//console.log(app)
+const arr=[2,3,4,5,6,3,4,4];
+
+//arr.filter((item)=>{
+  //  console.log(item)
+
+//}
+
+//)
+
+//let result =arr.filter((item)=>{
+  //  return item===4
+
+//}
+
+//)
+
+let result1 = arr.filter((item)=>{
+    
+    return item>2
+}
+
+)
+console.log(result1)
+
+// -------------- video 5----------
